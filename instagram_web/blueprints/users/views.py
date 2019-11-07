bd875cfce7ad689391a6b84e4b9fa28ca25b5af9
desmondsimzh <desmondsimzh@gmail.com>
2019-11-07 18:22:35 +0800

    new user form

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,19 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
 
-
+# /users/new
 @users_blueprint.route('/new', methods=['GET'])
 def new():
+    #render_template is for you to display that html
     return render_template('users/new.html')
 
-
-@users_blueprint.route('/', methods=['POST'])
+# /users/
+@users_blueprint.route('/', methods=['POST']) #use POST request to submit the form
 def create():
-    pass
+    # Form data is sent to this route
+    
+    # Get form data
+    
+    # Create a new user from that data
+
+    # Use pw to create a new instance of a user
+    # Save it inside the db
+    username = request.form.get('username')
+
+
+    print(username)
+
+    return redirect(url_for('users.new'))
+   
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
--- models/user.py
+++ models/user.py
@@ -1,6 +1,9 @@
 from models.base_model import BaseModel
+from werkzeug.security import generate_password_hash
 import peewee as pw
 
 
 class User(BaseModel):
     name = pw.CharField(unique=False)
+    password = pw.CharField()
+    email = pw.CharField(unique=True)
